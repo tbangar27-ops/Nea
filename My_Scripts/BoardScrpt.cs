a8a267063ecb3381e171602293718c06bf46888e
tbangar27-ops <tbangar27@gmail.com>
2026-03-09 19:40:26 +0000

    Add comments to BoardScrpt methods
    Added comments to clarify functionality of methods.

--- My_Scripts/BoardScrpt.cs
+++ My_Scripts/BoardScrpt.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 public class BoardScrpt : MonoBehaviour
@@ -5,20 +6,23 @@ public class BoardScrpt : MonoBehaviour
     public GameBehaviour.DoorType MyDtype;
     private Collider playerCollider;
     private bool isPlayer = false;
-
+    /* Records object entering hitbox */
     public void OnTriggerEnter(Collider other) {
+     /* Checks if object in hitbox is the player */
         if (other.CompareTag("Player")) {
             isPlayer = true;
             playerCollider = other;
         }
     }
-
+     /* Sets record to null when object leaves */
     public void OnTriggerExit(Collider other) {
+     /* Checks if object in hitbox is the player */
         if (other.CompareTag("Player")) {
             isPlayer = false;
             playerCollider = null;
         }
     }
+     /* Checks if player has the blue key when E is pressed, if they do destroys its parent object */
     public void Update() {
 if (Input.GetKeyDown(KeyCode.E) && isPlayer)
             {
@@ -29,3 +33,4 @@ if (Input.GetKeyDown(KeyCode.E) && isPlayer)
             }
         }
     }
+
